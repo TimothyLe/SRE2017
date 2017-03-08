d9b0ee87968f60a22b8f7f40f6bd6faec1f643c1
Thinh Le <timothyle85@gmail.com>
2017-03-08 11:05:05 -0800

    Update eeprom architecture
    Added Rusty's architecture

--- tim_stuff/eeprom.c
+++ tim_stuff/eeprom.c
@@ -1,29 +1,110 @@
-#include <stdio.h>
-#include "IO_EEPROM.h"
 #include "IO_Driver.h"
-#include "IO_CAN.h"
-#include "eeprom.h"
-using namespace std;
 
-/*Definitions*/
-void EEPROM::readEP(ubyte2 offset, ubyte2 length, ubyte1 data){
-    //check if the EEPROM is busy
-    if(IO_EEPROM_GetStatus() == IO_E_OK){
-        //not busy starts reading
-        IO_EEPROM_Read(offset, length, &data);
-    }
-    //data is busy not available
-    //needs IO_EEPROM_GetStatus 
-    //to return IO_E_OK 
+#include "eeprom.h"
+
+#define LOCAL static
+
+LOCAL bool getAddress(eepromValue value, ubyte2* address, ubyte1* bytes);
+LOCAL ? readInitialValues(?);
+
+//---------------------------------------------------------------
+// Constructor
+//---------------------------------------------------------------
+/**
+* @brief 
+*
+* @param[in]   value       Which EEPROM value we want the address/length of
+* @param[out]  address     The EEPROM address of the desired value
+* @param[out]  bytes       The length (number of bytes) of the value
+* @return      Whether or not the address/length were successfully found.
+*/
+EEPROMManager* EEPROMManager_new()
+{
+    ubyte2 size = 34;  //bytes
+
+    //Allocate memory for EEPROMManager object
+    EEPROMManager* me = (EEPROMManager*)malloc(sizeof(struct _EEPROMManager));
+
+    //Allocate memory for EEPROM data (2x)
+    me->data_actual = (ubyte1*)malloc(sizeof(ubyte1) * me->size);
+    me->data_desired = (ubyte1*)malloc(sizeof(ubyte1) * me->size);
+
+    me->status = EEPROM_op_initialize;
+
+    //Start initial EEPROM read
+    readInitialValues();
+    
+    return me;
 }
 
-void EEPROM::writeEP(ubyte2 offset, ubyte2 length, ubyte1 * data){
-     //check if the EEPROM is busy
-    if(IO_EEPROM_GetStatus() == IO_E_OK){
-        //not busy starts reading
-        IO_EEPROM_Write(offset, length, data);
-    }
-    //data is busy not available
-    //needs IO_EEPROM_GetStatus 
-    //to return IO_E_OK 
+//---------------------------------------------------------------
+// Mutators
+//---------------------------------------------------------------
+/**  @ingroup Mutators
+* @brief Specific description
+*/
+bool EEPROMManager_set_ubyte1(EEPROMManager* me, eepromValue parameter, ubyte1* value)
+{
+
+}
+
+//---------------------------------------------------------------
+// Accessors
+//---------------------------------------------------------------
+/**  @ingroup Accessors
+* @brief Specific description
+*/
+bool EEPROMManager_get_ubyte1(EEPROMManager* me, eepromValue parameter, ubyte1* value)
+{
+
+}
+
+
+//---------------------------------------------------------------
+// Special Functions
+//---------------------------------------------------------------
+IO_ErrorType EEPROMManager_sync(EEPROMManager* me)
+{
+    //
+}
+
+
+//---------------------------------------------------------------
+// Helper functions (private)
+//---------------------------------------------------------------
+/**
+ * @brief Returns the address and length of any variable in the eepromValue enum
+ *
+ * @param[in]   value       Which EEPROM value we want the address/length of
+ * @param[out]  address     The EEPROM address of the desired value
+ * @param[out]  bytes       The length (number of bytes) of the value
+ * @return      Whether or not the address/length were successfully found.
+ */
+LOCAL bool getAddress(eepromValue value, ubyte2* address, ubyte1* bytes)
+{
+    bool success = FALSE;
+
+
+    *address = 0;
+    *bytes = 0;
+    return success;
+}
+
+//Reads EEPROM and stores data in data_actual.  Waits until read is complete.
+LOCAL ? readInitialValues(???)
+{
+    //Read eeprom
+    //Loop until status == ok
+    me->status = EEPROM_op_idle;
+
+}
+
+LOCAL read(? ? ? )
+{
+    IO_EEPROM_Read()
+}
+
+LOCAL write(? ? ? )
+{
+
 }
